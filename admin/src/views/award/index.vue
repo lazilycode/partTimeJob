acef7e98a02f64f89a5a773a25c37ae527a6ae92
lazycode <wkl13022195532@gmail.com>
2019-12-15 00:20:58 +0800

    no message

--- admin/src/views/award/index.vue
+++ admin/src/views/award/index.vue
@@ -8,88 +8,88 @@
               <el-input v-model="input" placeholder="请输入内容" size="mini" />
             </el-col>
             <el-col :span="6">
-              <el-button size="mini" @click="add()"  > 创建 </el-button>
+              <el-button size="mini" @click="add()"> 创建 </el-button>
             </el-col>
           </el-row>
           <div v-for="(o, index) in arr" :key="index" class="text item">
-            <div v-bind:class="{ active: o.action }" @click='click_row(index)'> 
-            <el-button size="mini" type="text" >{{ index+' '+ o.name }}</el-button>
-              </div>
+            <div :class="{ active: o.action }" @click="click_row(index)">
+              <el-button size="mini" type="text">{{ index+' '+ o.name }}</el-button>
+            </div>
           </div>
         </el-card>
       </el-col>
       <el-col :span="15">
-        <el-card >
-        <el-row>
-          <el-button >生 效</el-button>
-          <el-button type="primary" > 失生 效</el-button>
-          <el-button type="success" >删除</el-button>
-          <el-button type="info"  >编辑</el-button>
-        </el-row>
+        <el-card>
+          <el-row>
+            <el-button>生 效</el-button>
+            <el-button type="primary"> 失生 效</el-button>
+            <el-button type="success">删除</el-button>
+            <el-button type="info">编辑</el-button>
+          </el-row>
           <br>
-          <el-row>  
-           <el-card> 
-            <el-row>
-                <el-col :span="4">  
-                奖项规则：
+          <el-row>
+            <el-card>
+              <el-row>
+                <el-col :span="4">
+                  奖项规则：
                 </el-col>
                 <el-col :span="12">
-                <el-input v-model="prize" placeholder=""></el-input>
+                  <el-input v-model="prize" placeholder="" />
                 </el-col>
-            </el-row>
+              </el-row>
               <br>
-             <el-row>
-                <el-col :span="4">  
-                规则类型：
+              <el-row>
+                <el-col :span="4">
+                  规则类型：
                 </el-col>
                 <el-col :span="12">
-                <el-select v-model="value" placeholder="请选择">
-                  <el-option
-                    v-for="item in options"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value">
-                  </el-option>
-                </el-select>
+                  <el-select v-model="value" placeholder="请选择">
+                    <el-option
+                      v-for="item in options"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                    />
+                  </el-select>
                 </el-col>
-            </el-row>
-             <br>
-             <el-row>
-                <el-col :span="4">  
-                奖项：
-                </el-col>
-                <el-col :span="12">
-                <el-select v-model="value" placeholder="请选择">
-                  <el-option
-                    v-for="item in options"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value">
-                  </el-option>
-                </el-select>
-                </el-col>
-            </el-row>
-            <br>
-             <el-row>
-                <el-col :span="4">  
-                奖项剩余数量：
+              </el-row>
+              <br>
+              <el-row>
+                <el-col :span="4">
+                  奖项：
                 </el-col>
                 <el-col :span="12">
-                <el-select v-model="value" placeholder="请选择">
-                  <el-option
-                    v-for="item in options"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value">
-                  </el-option>
-                </el-select>
+                  <el-select v-model="value" placeholder="请选择">
+                    <el-option
+                      v-for="item in options"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                    />
+                  </el-select>
                 </el-col>
-            </el-row>
-           </el-card>
+              </el-row>
+              <br>
+              <el-row>
+                <el-col :span="4">
+                  奖项剩余数量：
+                </el-col>
+                <el-col :span="12">
+                  <el-select v-model="value" placeholder="请选择">
+                    <el-option
+                      v-for="item in options"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                    />
+                  </el-select>
+                </el-col>
+              </el-row>
+            </el-card>
           </el-row>
         </el-card>
       </el-col>
-    </el-row> 
+    </el-row>
   </div>
 </template>
 
@@ -109,31 +109,31 @@ export default {
   },
   data() {
     return {
-      prize:'而低等级爱哪个',
+      prize: '而低等级爱哪个',
       arr: [
-        { name: '奖项规则', value: '1',disabled:true ,id:'1',action:true},
-        { name: '奖项规则', value: '2', disabled:true ,id:'2'},
-        { name: '奖项规则', value: '3', disabled:true ,id:'3'},
-        { name: '奖项规则', value: '4',disabled:true ,id:'4'}
+        { name: '奖项规则', value: '1', disabled: true, id: '1', action: true },
+        { name: '奖项规则', value: '2', disabled: true, id: '2' },
+        { name: '奖项规则', value: '3', disabled: true, id: '3' },
+        { name: '奖项规则', value: '4', disabled: true, id: '4' }
       ],
       isAdd: false,
       options: [{
-          value: '选项1',
-          label: '黄金糕'
-        }, {
-          value: '选项2',
-          label: '双皮奶'
-        }, {
-          value: '选项3',
-          label: '蚵仔煎'
-        }, {
-          value: '选项4',
-          label: '龙须面'
-        }, {
-          value: '选项5',
-          label: '北京烤鸭'
-        }],
-        value: ''
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value: ''
     }
   },
   created() {
@@ -146,13 +146,13 @@ export default {
     },
 
     // 点击标示
-    click_row(e){
-      return this.arr.map((item,index)=>{
-       if(e===index){
-         this.$set(item,'action',true)
-       }else{
-          this.$set(item,'action',false)
-       }
+    click_row(e) {
+      return this.arr.map((item, index) => {
+        if (e === index) {
+          this.$set(item, 'action', true)
+        } else {
+          this.$set(item, 'action', false)
+        }
       })
     }
   }
--- admin/src/views/censorship/index.vue
+++ admin/src/views/censorship/index.vue
@@ -12,104 +12,101 @@
             </el-col>
           </el-row>
           <div v-for="(o, index) in arr" :key="index" class="text item">
-            <div v-bind:class="{ active: o.action }" @click='click_row(index)'> <el-button size="mini" type="text" >{{ index+' '+ o.name }}</el-button> <el-button size="mini" type="text"  @click="dialogVisible = true">导入</el-button> <el-button size="mini" type="text" @click='startUsing' :disabled='o.disabled' >启用</el-button><el-button size="mini" type="text">配置</el-button></div>
+            <div :class="{ active: o.action }" @click="click_row(index)"> <el-button size="mini" type="text">{{ index+' '+ o.name }}</el-button> <el-button size="mini" type="text" @click="dialogVisible = true">导入</el-button> <el-button size="mini" type="text" :disabled="o.disabled" @click="startUsing">启用</el-button><el-button size="mini" type="text">配置</el-button></div>
           </div>
         </el-card>
       </el-col>
       <el-col :span="19">
         <div>
-       <el-row>
-          <el-col :span="24">
-           <el-button type="primary" @click='creatTopic(0)'>创建题目</el-button>
-          </el-col>
-        </el-row>
-
-
-          <el-row class="row_flag" v-for="(item, index) in arrTopic" :key="index">
-            <el-col :span="24" class="row_list">
-              <el-row>
-                  <el-col :span="20">
-                     <p class="row_ti_name"><span class="row_ti_name_sole">{{index}}</span> <span> {{item.topicName}}</span></p>
-                      <div class="maben_left">
-                        <p class="row_ti_name height_fen"><span>(最高分值{{item.score}})</span></p>
-                        <div>
-                          <el-checkbox-group v-model="item.checklist">
-                            <p v-for='items in item.result' :label="items.index" :key="items.index">
-                              <el-checkbox :label="items.index"  >{{items.resultName}}</el-checkbox>
-                            </p>
-                          </el-checkbox-group>
-                        </div>
-                      </div>
-                    </el-col>
-                  <el-col :span="4">
-                     <el-button type="primary" size="small" @click='editTopic(index)'>编辑</el-button>
-                     <el-button type="primary" size="small" @click='creatTopic'>删除</el-button>
-                  </el-col>
-                </el-row>
+          <el-row>
+            <el-col :span="24">
+              <el-button type="primary" @click="creatTopic(0)">创建题目</el-button>
             </el-col>
           </el-row>
 
-        
-          
+          <el-row v-for="(item, index) in arrTopic" :key="index" class="row_flag">
+            <el-col :span="24" class="row_list">
+              <el-row>
+                <el-col :span="20">
+                  <p class="row_ti_name"><span class="row_ti_name_sole">{{ index }}</span> <span> {{ item.topicName }}</span></p>
+                  <div class="maben_left">
+                    <p class="row_ti_name height_fen"><span>(最高分值{{ item.score }})</span></p>
+                    <div>
+                      <el-checkbox-group v-model="item.checklist">
+                        <p v-for="items in item.result" :key="items.index" :label="items.index">
+                          <el-checkbox :label="items.index">{{ items.resultName }}</el-checkbox>
+                        </p>
+                      </el-checkbox-group>
+                    </div>
+                  </div>
+                </el-col>
+                <el-col :span="4">
+                  <el-button type="primary" size="small" @click="editTopic(index)">编辑</el-button>
+                  <el-button type="primary" size="small" @click="creatTopic">删除</el-button>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+
         </div>
       </el-col>
     </el-row>
-
 
     <el-dialog
       title="导入excel"
       :visible.sync="dialogVisible"
       width="50%"
-      >
+    >
       <el-upload
         class="avatar-uploader"
         action="https://jsonplaceholder.typicode.com/posts/"
         :show-file-list="false"
         :on-success="handleAvatarSuccess"
-        :before-upload="beforeAvatarUpload">
+        :before-upload="beforeAvatarUpload"
+      >
         <img v-if="imageUrl" :src="imageUrl" class="avatar">
-        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        <i v-else class="el-icon-plus avatar-uploader-icon" />
       </el-upload>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
       </span>
     </el-dialog>
 
-   <el-dialog
-    title="提示"
-    :visible.sync="topicdialogVisible"
-    width="40%"
+    <el-dialog
+      title="提示"
+      :visible.sync="topicdialogVisible"
+      width="40%"
     >
-   <el-row>
-   <div class="row_falg">
-   <p> 每道题{{creatArr.score}}分</p>
-   <el-input type="textarea" :rows="2" placeholder="请输入题目内容" v-model="creatArr.topicName"/>
-   </div>
-  </el-input>
-    <el-row :gutter="20">
-        <div v-for='(item,index) in creatArr.result' class="row_falg">
+      <el-row>
+        <div class="row_falg">
+          <p> 每道题{{ creatArr.score }}分</p>
+          <el-input v-model="creatArr.topicName" type="textarea" :rows="2" placeholder="请输入题目内容" />
+        </div>
+        </el-input>
+        <el-row :gutter="20">
+          <div v-for="(item,index) in creatArr.result" class="row_falg">
             <el-col :span="10">
-            <el-input v-model="item.resultName" placeholder="请输入答案"></el-input> 
+              <el-input v-model="item.resultName" placeholder="请输入答案" />
             </el-col>
             <el-col :span="5">
-              <el-checkbox v-model="item.defaultResult">备选项</el-checkbox>  
+              <el-checkbox v-model="item.defaultResult">备选项</el-checkbox>
             </el-col>
-            <el-col :span="4" >
-              <el-button type="primary"  v-if='index===0' @click='addTopic'> 增加</el-button>
+            <el-col :span="4">
+              <el-button v-if="index===0" type="primary" @click="addTopic"> 增加</el-button>
             </el-col>
-             <el-col :span="4" >
-              <el-button type="primary" v-if='index!==0'  @click='delTopic(index)'> 删除</el-button>
+            <el-col :span="4">
+              <el-button v-if="index!==0" type="primary" @click="delTopic(index)"> 删除</el-button>
             </el-col>
-        </div>
-        <br>
+          </div>
+          <br>
+        </el-row>
       </el-row>
-    </el-row>
-    <span slot="footer" class="dialog-footer">
-      <el-button @click="topicdialogVisible = false">取 消</el-button>
-      <el-button type="primary" v-if='!edit' @click='sure'>确 定</el-button>
-      <el-button type="primary" v-if='edit' @click='editSure(creatArr.result)'>编辑确定</el-button>
-    </span>
-  </el-dialog>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="topicdialogVisible = false">取 消</el-button>
+        <el-button v-if="!edit" type="primary" @click="sure">确 定</el-button>
+        <el-button v-if="edit" type="primary" @click="editSure(creatArr.result)">编辑确定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -129,31 +126,30 @@ export default {
   },
   data() {
     return {
-      topIc:'',
-      //创建题目
-      topicdialogVisible:false,
+      topIc: '',
+      // 创建题目
+      topicdialogVisible: false,
       // 导入
       dialogVisible: false,
-       imageUrl: '',
+      imageUrl: '',
       input: '',
-      //创建题目
-      edit:false,
-      creatArr:{
-        score:10,
-        topicName:'',
-        result:[{
-          resultName:'',
-          defaultResult:''
+      // 创建题目
+      edit: false,
+      creatArr: {
+        score: 10,
+        topicName: '',
+        result: [{
+          resultName: '',
+          defaultResult: ''
         }]
       },
-      arrTopic:[],
-      
+      arrTopic: [],
 
       arr: [
-        { name: '第一题', value: '1',disabled:true ,id:'1',action:true},
-        { name: '第二题', value: '2', disabled:true ,id:'2'},
-        { name: '第三题', value: '3', disabled:true ,id:'3'},
-        { name: '第四题', value: '4',disabled:true ,id:'4'}
+        { name: '第一题', value: '1', disabled: true, id: '1', action: true },
+        { name: '第二题', value: '2', disabled: true, id: '2' },
+        { name: '第三题', value: '3', disabled: true, id: '3' },
+        { name: '第四题', value: '4', disabled: true, id: '4' }
       ],
       isAdd: false,
       list: null,
@@ -179,89 +175,88 @@ export default {
       })
     },
     // 点击标示
-    click_row(e){
-      return this.arr.map((item,index)=>{
-       if(e===index){
-         this.$set(item,'action',true)
-       }else{
-          this.$set(item,'action',false)
-       }
+    click_row(e) {
+      return this.arr.map((item, index) => {
+        if (e === index) {
+          this.$set(item, 'action', true)
+        } else {
+          this.$set(item, 'action', false)
+        }
       })
     },
     // 倒入excel
-    importExcol(){
-      
+    importExcol() {
+
     },
     handleAvatarSuccess(res, file) {
-        this.imageUrl = URL.createObjectURL(file.raw);
-        this.dialogVisible=false
-      },
+      this.imageUrl = URL.createObjectURL(file.raw)
+      this.dialogVisible = false
+    },
     beforeAvatarUpload(file) {
-      const isJPG = file.type === 'image/jpeg';
-      const isLt2M = file.size / 1024 / 1024 < 2;
+      const isJPG = file.type === 'image/jpeg'
+      const isLt2M = file.size / 1024 / 1024 < 2
 
       if (!isJPG) {
-        this.$message.error('上传头像图片只能是 JPG 格式!');
+        this.$message.error('上传头像图片只能是 JPG 格式!')
       }
       if (!isLt2M) {
-        this.$message.error('上传头像图片大小不能超过 2MB!');
+        this.$message.error('上传头像图片大小不能超过 2MB!')
       }
-      return isJPG && isLt2M;
+      return isJPG && isLt2M
     },
     // 开启接口
-    startUsing(){
+    startUsing() {
       level_openOrClose().then(response => {
         this.list = response.data.items
         this.listLoading = false
       })
     },
     // 创建题目
-    creatTopic(){
+    creatTopic() {
       this.topicdialogVisible = true
-      this.creatArr={score:10,
-        topicName:'',
-        result:[{
-          resultName:'',
-          defaultResult:''
-        }]}
+      this.creatArr = { score: 10,
+        topicName: '',
+        result: [{
+          resultName: '',
+          defaultResult: ''
+        }] }
     },
     // 编辑
-    editTopic(index){
-      this.topIc=index
-      this.edit=true
+    editTopic(index) {
+      this.topIc = index
+      this.edit = true
       this.topicdialogVisible = true
-      this.creatArr=this.arrTopic[index]
-       
+      this.creatArr = this.arrTopic[index]
     },
     // 增加答案条数
-    addTopic(){
-        this.creatArr.result.push({
-          resultName:'',
-          defaultResult:''
-        })
+    addTopic() {
+      this.creatArr.result.push({
+        resultName: '',
+        defaultResult: ''
+      })
     },
-    delTopic(index){
+    delTopic(index) {
       this.creatArr.result.splice(index, 1)
     },
     // 创建题目 点击  确定
-    sure(){
-      this.creatArr.checklist=this.creatArr.result.map((item,index)=>{
-         item.index=index
-        if(item.defaultResult){
+    sure() {
+      this.creatArr.checklist = this.creatArr.result.map((item, index) => {
+        item.index = index
+        if (item.defaultResult) {
           return item.index
         }
       })
 
-       this.creatArr.checklist=this.creatArr.checklist.filter(function(val){
-          return !(val === ""||val ===undefined);
-        })
+      this.creatArr.checklist = this.creatArr.checklist.filter(function(val) {
+        return !(val === '' || val === undefined)
+      })
 
       this.arrTopic.push(this.creatArr)
       this.topicdialogVisible = false
-    } ,
+    },
     // 编辑 点击确定
-    editSure(arr){
-       this.topicdialogVisible = false
+    editSure(arr) {
+      this.topicdialogVisible = false
       // this.arrTopic[this.topIc]=this.creatArr
       // this.arrTopic.splice()
     }
