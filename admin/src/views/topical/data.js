58809b1e4e8ffb4a5d97e283230d7c0e8f0b2a5a
lazycode <wkl13022195532@gmail.com>
2019-12-15 11:45:42 +0800

    add code

---
+++ admin/src/views/topical/data.js
@@ -0,0 +1,25 @@
+
+// 获取节日主题
+const tableData = [
+  {
+    ID: '1',
+    name: '节日名称',
+    bg: '背景色',
+    bgIMg: '背景图',
+    sybgIMg: '首页背景图',
+    tiBgimg:'题目背景',
+    daAnimg:'答案背景'
+  }
+]
+
+
+// 提供
+// 发送参数
+{
+    name:'',
+    bg:'',
+    bgUrl,
+    sybgIMgUrl:'',
+    tiBgimgUrl:'',
+    daAnimg:''
+}
